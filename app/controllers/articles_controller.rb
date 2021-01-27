a3f9e9bda8cfb167e4d78141d5cf1098e7f5b211
Kota Kanazawa <kota0818@gmail.com>
2021-01-27 20:55:30 +0900

    ルーティングのscopeを試す

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,5 @@
+class ArticlesController < ApplicationController
+  def index
+    @article = '記事やで！'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  scope :blog do
+    resources :articles, only: :index
+  end
   root "users#index"
   resources :users
 end
