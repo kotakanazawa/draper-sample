2784c0d3cfa0fe0fbb25369c37ed757aedf689ea
Kota Kanazawa <kota0818@gmail.com>
2021-02-02 20:21:57 +0900

    Learn delegate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  has_one :profile
+
+  delegate :food, to: :profile
+  # これを delegate メソッドで使えるようにしている
+  # def food
+  #   profile.food
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_105504) do
+ActiveRecord::Schema.define(version: 2021_02_02_111000) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "food"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,4 +26,5 @@ ActiveRecord::Schema.define(version: 2021_01_21_105504) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
